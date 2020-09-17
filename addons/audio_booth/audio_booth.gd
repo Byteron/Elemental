455d5c6054dbabcf656899f4e7624ccfd5fd6d89
Aaron Winter <winter.aaron93@gmail.com>
2020-09-17 15:59:06 +0200

    fixed errors by using absolute paths

--- addons/audio_booth/audio_booth.gd
+++ addons/audio_booth/audio_booth.gd
@@ -2,14 +2,14 @@ tool
 extends EditorPlugin
 
 func _enter_tree() -> void:
-	add_custom_type("MusicBooth", "Node", load("source/music/MusicBooth.gd"), load("graphics/icons/icon.png"))
-	add_custom_type("Song", "Node", load("source/music/Song.gd"), load("graphics/icons/icon.png"))
+	add_custom_type("MusicBooth", "Node", load("addons/audio_booth/source/music/MusicBooth.gd"), load("addons/audio_booth/graphics/icons/icon.png"))
+	add_custom_type("Song", "Node", load("addons/audio_booth/source/music/Song.gd"), load("addons/audio_booth/graphics/icons/icon.png"))
 
-	add_custom_type("TrackContainer", "Node", load("source/music/containers/TrackContainer.gd"), load("graphics/icons/icon.png"))
-	add_custom_type("StingerContainer", "Node", load("source/music/containers/StingerContainer.gd"), load("graphics/icons/icon.png"))
+	add_custom_type("TrackContainer", "Node", load("addons/audio_booth/source/music/containers/TrackContainer.gd"), load("addons/audio_booth/graphics/icons/icon.png"))
+	add_custom_type("StingerContainer", "Node", load("addons/audio_booth/source/music/containers/StingerContainer.gd"), load("addons/audio_booth/graphics/icons/icon.png"))
 
-	add_custom_type("SoundBooth", "Node", load("source/sfx/SoundBooth.gd"), load("graphics/icons/icon.png"))
-	add_custom_type("Sound", "Node", load("source/sfx/Sound.gd"), load("graphics/icons/icon.png"))
+	add_custom_type("SoundBooth", "Node", load("addons/audio_booth/source/sfx/SoundBooth.gd"), load("addons/audio_booth/graphics/icons/icon.png"))
+	add_custom_type("Sound", "Node", load("addons/audio_booth/source/sfx/Sound.gd"), load("addons/audio_booth/graphics/icons/icon.png"))
 
 func _exit_tree() -> void:
 	remove_custom_type("MusicBooth")
