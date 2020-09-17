2fa9110bea55a20cb765ffd831fcb14d6d2271f2
Aaron Winter <winter.aaron93@gmail.com>
2020-09-17 20:51:51 +0200

    terrain tile movement

--- source/terrain/Terrain.gd
+++ source/terrain/Terrain.gd
@@ -7,6 +7,9 @@ enum Action {
 	FALL,
 }
 
+export var height := 0.0
+export var speed := 0.0
+
 export var alias := ""
 export var fertile := false
 export var brittle := false
@@ -16,9 +19,15 @@ export(Array, Resource) var transitions = []
 var cell := Vector3()
 var position := Vector3()
 
+onready var tween := $Tween as Tween
+
 
 func _ready() -> void:
 	rotation_degrees.y = [0, 90, 180, 270][randi() % 4]
+	yield(get_tree().create_timer(randf()), "timeout")
+	tween.interpolate_property(self, "transform:origin:y", transform.origin.y, transform.origin.y - height, speed / 2, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+	tween.interpolate_property(self, "transform:origin:y", transform.origin.y - height,transform.origin.y, speed / 2, Tween.TRANS_SINE, Tween.EASE_IN_OUT, speed / 2)
+	tween.start()
 
 
 func on_moved(map) -> void:
@@ -35,3 +44,8 @@ func _on_moved(map) -> void:
 
 func _is_blocked(state: String) -> bool:
 	return true
+
+
+func _set_height(value: float) -> void:
+	height = value
+	transform.origin.y = value
