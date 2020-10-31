3e55596eaa78c3c708ab5febeac5ac6fb49543f9
Aaron Winter <winter.aaron93@gmail.com>
2020-10-31 10:58:11 +0100

    removed unused OrbMaterialData class

--- source/objects/orb/OrbMaterialData.gd
+++
@@ -1,7 +0,0 @@
-extends Resource
-class_name OrbMaterialData
-
-export var fire : Material = null
-export var ice : Material = null
-export var stone : Material = null
-export var wind : Material = null
