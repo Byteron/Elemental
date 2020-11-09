1604e9be8c40bff0718e4d7c3dcece088716696f
Aaron Winter <winter.aaron93@gmail.com>
2020-11-09 18:23:25 +0100

    moving game logic from map to game. Fixes #10

--- source/game/Game.gd
+++ source/game/Game.gd
@@ -4,9 +4,14 @@ class_name Game
 export var size := Vector2(10, 10)
 export var random := false
 
+var steps := 0
+
+var earth_block_count := 0
+var seeds_planted := 0
+
 var behaviors := {}
 
-var steps := 0
+var tick_calls := {}
 
 onready var elemental := $Elemental as Elemental
 onready var map := $Map as Map
@@ -58,20 +63,7 @@ func _process(delta: float) -> void:
 		map.move_elemental(Vector3.RIGHT)
 
 
-func execute_behaviors() -> void:
-	for behavior in behaviors.values():
-		behavior.execute()
-
-
-func _on_Map_creature_added(creature: Creature) -> void:
-	behaviors[creature.behavior].add_creature(creature)
-
-
-func _on_Map_pretick() -> void:
-	execute_behaviors()
-
-
-func _on_Map_finished() -> void:
+func _finish() -> void:
 	print("Rating: %f" % (float(map.optimal_steps) / float(steps) * 100.0))
 	set_process(false)
 	set_process_unhandled_input(false)
@@ -80,6 +72,156 @@ func _on_Map_finished() -> void:
 	Global.next_level()
 
 
-func _on_Map_tick() -> void:
+func _execute_behaviors() -> void:
+	for behavior in behaviors.values():
+		behavior.execute()
+
+
+func _tick() -> void:
+	tick_calls.clear()
+
+	_tick_analyze()
+	_tick_apply()
+
 	steps += 1
 	step_counter_label.text = "Steps: %d / %d" % [steps, map.optimal_steps]
+
+
+func _tick_analyze() -> void:
+	_tick_analyze_conduction()
+	_tick_analyze_interactions()
+
+
+func _tick_analyze_conduction() -> void:
+	var conductions := {}
+
+	for element in Entity.Element.values():
+		conductions[element] = []
+
+	for cell in map.locations:
+		var loc: Location = map.locations[cell]
+
+		loc.broadcast.clear()
+
+		for element in loc.get_conduction():
+			conductions[element].append(loc)
+
+	for element in conductions:
+		var locs : Array = conductions[element]
+
+		var is_conducting := false
+
+		for loc in locs:
+			for n_loc in map.get_neighbors(loc):
+				if n_loc.get_broadcast().has(element):
+					is_conducting = true
+					break
+
+		if is_conducting:
+			for loc in locs:
+				loc.broadcast.append(element)
+
+
+func _tick_analyze_interactions() -> void:
+	for cell in map.locations:
+		var loc: Location = map.locations[cell]
+
+		if not tick_calls.has(loc):
+			tick_calls[loc] = []
+
+		for element in loc.get_broadcast():
+			tick_calls[loc].append(element)
+
+		for n_loc in map.get_neighbors(loc):
+
+			if not tick_calls.has(n_loc):
+				tick_calls[n_loc] = []
+
+			for element in loc.get_broadcast():
+				tick_calls[n_loc].append(element)
+
+
+func _tick_apply() -> void:
+	for loc in tick_calls:
+		var elements: Array = tick_calls[loc]
+
+		for element in elements:
+			loc.call("_" + Entity.Element.keys()[element].to_lower(), false)
+
+
+func _check_conditions() -> void:
+	var blocks_left = 0
+
+	for cell in map.locations:
+		var loc : Location = map.locations[cell]
+
+		if loc.terrain.is_fertile:
+			blocks_left += 1
+
+	if not blocks_left:
+		_finish()
+
+
+func _check_brittle_terrain(loc: Location) -> void:
+	if loc.terrain.is_brittle:
+		loc.change_terrain("None")
+
+
+func _check_collecting_orb(loc: Location) -> void:
+	if loc.orb:
+		if [Entity.Element.ICE, Entity.Element.FIRE].has(loc.orb.element) and elemental.seeds:
+			elemental.seeds = 0
+
+		elemental.state = loc.orb.element
+		elemental.plop()
+		loc.orb.collect()
+
+
+func _check_sigil(loc: Location) -> void:
+
+	if loc.sigil and elemental.state == loc.sigil.element:
+		return
+
+	if loc.sigil:
+		if [Entity.Element.ICE, Entity.Element.FIRE].has(loc.sigil.element) and elemental.seeds:
+			elemental.seeds = 0
+
+		elemental.state = loc.sigil.element
+		elemental.plop()
+		loc.sigil.activate()
+
+
+func _check_collecting_seeds(loc: Location) -> void:
+	if loc.seeds:
+		elemental.seeds += 1
+		loc.seeds.collect()
+
+
+func _check_planting_seeds(loc: Location) -> void:
+	if elemental.seeds and loc.terrain and loc.terrain.is_fertile:
+		elemental.seeds -= 1
+		seeds_planted += 1
+		print("Seeds - 1")
+		loc.change_terrain("Grass")
+		SFX.play_sfx("Plant")
+
+
+func _on_Map_elemental_move_finished(last_loc: Location, loc: Location) -> void:
+	_check_collecting_orb(loc)
+	_check_sigil(loc)
+
+	_check_brittle_terrain(last_loc)
+	_check_collecting_seeds(loc)
+	_check_planting_seeds(loc)
+
+	_tick()
+
+	call_deferred("_check_conditions")
+
+
+func _on_Map_creature_added(creature: Creature) -> void:
+	behaviors[creature.behavior].add_creature(creature)
+
+
+func _on_Map_elemental_moving() -> void:
+	_execute_behaviors()
--- source/map/Map.gd
+++ source/map/Map.gd
@@ -10,12 +10,12 @@ const NEIGHBORS = [
 	Vector3(0, 0, -1)
 ]
 
-signal tick()
-signal pretick()
-signal finished()
-signal game_over()
 signal cell_hovered(cell)
+
 signal creature_added(creature)
+
+signal elemental_moving()
+signal elemental_move_finished(last_loc, loc)
 
 var size := Vector2(0, 0)
 
@@ -26,11 +26,6 @@ var elemental : Elemental = null
 
 var locations := {}
 var character_locations := {}
-
-var tick_calls := {}
-
-var earth_block_count := 0
-var seeds_planted := 0
 
 var optimal_steps := 0
 
@@ -91,15 +86,6 @@ func randomize_terrain() -> void:
 			add_sigil(loc.cell, Global.sigils.keys()[randi() % Global.sigils.size()])
 		elif randf() < 0.04:
 			add_seeds(loc.cell)
-
-
-func tick() -> void:
-	tick_calls.clear()
-
-	_tick_analyze()
-	_tick_apply()
-
-	emit_signal("tick")
 
 
 func get_neighbors(loc: Location) -> Array:
@@ -217,8 +203,6 @@ func add_creature(cell: Vector3, alias: String) -> void:
 
 	var creature = Global.creatures[alias].instance()
 	creatures.add_child(creature)
-
-	creature.connect("move_finished", self, "_on_creature_move_finished")
 
 	creature.cell = loc.cell
 	creature.transform.origin = loc.position
@@ -345,68 +329,6 @@ func get_map_data() -> MapData:
 	return map_data
 
 
-func _tick_analyze() -> void:
-	_tick_analyze_conduction()
-	_tick_analyze_interactions()
-
-
-func _tick_analyze_conduction() -> void:
-	var conductions := {}
-
-	for element in Entity.Element.values():
-		conductions[element] = []
-
-	for cell in locations:
-		var loc: Location = locations[cell]
-
-		loc.broadcast.clear()
-
-		for element in loc.get_conduction():
-			conductions[element].append(loc)
-
-	for element in conductions:
-		var locs : Array = conductions[element]
-
-		var is_conducting := false
-
-		for loc in locs:
-			for n_loc in get_neighbors(loc):
-				if n_loc.get_broadcast().has(element):
-					is_conducting = true
-					break
-
-		if is_conducting:
-			for loc in locs:
-				loc.broadcast.append(element)
-
-
-func _tick_analyze_interactions() -> void:
-	for cell in locations:
-		var loc: Location = locations[cell]
-
-		if not tick_calls.has(loc):
-			tick_calls[loc] = []
-
-		for element in loc.get_broadcast():
-			tick_calls[loc].append(element)
-
-		for n_loc in get_neighbors(loc):
-
-			if not tick_calls.has(n_loc):
-				tick_calls[n_loc] = []
-
-			for element in loc.get_broadcast():
-				tick_calls[n_loc].append(element)
-
-
-func _tick_apply() -> void:
-	for loc in tick_calls:
-		var elements: Array = tick_calls[loc]
-
-		for element in elements:
-			loc.call("_" + Entity.Element.keys()[element].to_lower(), false)
-
-
 func _add_location(alias: String, cell: Vector3, animate := true) -> void:
 	var terrain : Terrain = Global.terrains[alias].instance()
 	var loc := Location.new()
@@ -436,84 +358,12 @@ func _remove_location(cell) -> void:
 	locations.erase(cell)
 
 
-func _check_conditions() -> void:
-	var blocks_left = 0
-
-	for cell in locations:
-		var loc : Location = locations[cell]
-
-		if loc.terrain.is_fertile:
-			blocks_left += 1
-
-	if not blocks_left:
-		emit_signal("finished")
-
-
-func _check_brittle_terrain(loc: Location) -> void:
-	if loc.terrain.is_brittle:
-		loc.change_terrain("None")
-
-
-func _check_collecting_orb(loc: Location) -> void:
-	if loc.orb:
-		if [Entity.Element.ICE, Entity.Element.FIRE].has(loc.orb.element) and elemental.seeds:
-			elemental.seeds = 0
-
-		elemental.state = loc.orb.element
-		elemental.plop()
-		loc.orb.collect()
-
-
-func _check_sigil(loc: Location) -> void:
-
-	if loc.sigil and elemental.state == loc.sigil.element:
-		return
-
-	if loc.sigil:
-		if [Entity.Element.ICE, Entity.Element.FIRE].has(loc.sigil.element) and elemental.seeds:
-			elemental.seeds = 0
-
-		elemental.state = loc.sigil.element
-		elemental.plop()
-		loc.sigil.activate()
-
-
-func _check_collecting_seeds(loc: Location) -> void:
-	if loc.seeds:
-		elemental.seeds += 1
-		loc.seeds.collect()
-
-
-func _check_planting_seeds(loc: Location) -> void:
-	if elemental.seeds and loc.terrain and loc.terrain.is_fertile:
-		elemental.seeds -= 1
-		seeds_planted += 1
-		print("Seeds - 1")
-		loc.change_terrain("Grass")
-		SFX.play_sfx("Plant")
-
-
 func _on_terrain_hovered(loc: Location) -> void:
 	emit_signal("cell_hovered", loc.cell)
-
-
-func _on_creature_move_finished(creature: Character, last_cell: Vector3, new_cell: Vector3) -> void:
-	var last_loc: Location = locations[last_cell]
-	var loc : Location = locations[new_cell]
 
 
 func _on_elemental_move_finished(elemental: Character, last_cell: Vector3, new_cell: Vector3) -> void:
 	var last_loc: Location = locations[last_cell]
 	var loc : Location = locations[new_cell]
 
-	_check_collecting_orb(loc)
-	_check_sigil(loc)
-
-
-	_check_brittle_terrain(last_loc)
-	_check_collecting_seeds(loc)
-	_check_planting_seeds(loc)
-
-	tick()
-
-	call_deferred("_check_conditions")
+	emit_signal("elemental_move_finished", last_loc, loc)
