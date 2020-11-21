e59e692827d86c318b03606580c591b9ae5f7ed9
Aaron Winter <winter.aaron93@gmail.com>
2020-11-21 16:50:22 +0100

    remove debug prints from stray

--- data/behaviors/Stray.gd
+++ data/behaviors/Stray.gd
@@ -2,14 +2,10 @@ extends Behavior
 
 
 func _execute(map: Map, loc: Location, creature: Creature) -> void:
-	print(creature.walkable)
 	var walkable = map.find_walkable_locations(loc, creature.walkable)
 	var neighbors = map.get_neighbors(loc)
 
 	var next_loc = neighbors[randi() % neighbors.size()]
-
-	print(next_loc)
-	print(walkable)
 
 	if next_loc.get_terrain_alias() in walkable:
 		var __ = map.move_character(loc, next_loc)
