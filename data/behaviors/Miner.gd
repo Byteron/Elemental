1f3b8f5c4957dcaa42a3c9373edbef293e639e40
Aaron Winter <winter.aaron93@gmail.com>
2020-11-21 13:55:30 +0100

    fixed miner

--- data/behaviors/Miner.gd
+++ data/behaviors/Miner.gd
@@ -3,39 +3,37 @@ extends Behavior
 
 func _execute(map: Map, loc: Location, creature: Creature) -> void:
 
-	var reachable = map.find_walkable_locations(loc, creature.walkable)
+	var walkable = map.find_walkable_locations(loc, creature.walkable)
+	var ore_locs := map.find_adjacent_obstacles(walkable, "Ore")
 
-	var search_loc : Location = null
+	var ore_loc : Location = ore_locs.pop_front()
+	var candle_loc := map.find_location_with_item_from_reachable(walkable, "Candle")
 
 	if creature.has_candle:
-		var ore_locs := map.find_adjacent_obstacles(reachable, "Ore")
-		search_loc = ore_locs.pop_front()
+		if ore_loc:
+			if map.are_locations_neighbors(loc, ore_loc):
+				ore_loc.obstacle.destroy()
+			else:
+				map.move_character_with_path(loc, ore_loc)
+		else:
+			if loc.cell == creature.get_path_cell():
+				_follow_path(map, loc, creature)
+			else:
+				map.move_character_with_path(loc, map.get_location(creature.get_path_cell()))
 	else:
-		search_loc = map.find_location_with_item_from_reachable(reachable, "Candle")
-
-	if creature.has_candle and map.are_locations_neighbors(loc, search_loc):
-		search_loc.obstacle.destroy()
-	elif search_loc == loc:
-		loc.item.destroy()
-		creature.has_candle = true
-		creature.candle.visible = true
-		creature.candle._fire(false)
-	elif search_loc and not creature.has_candle:
-		if search_loc.item.is_lit:
-			map.move_character_with_path(loc, search_loc)
-	elif search_loc:
-			map.move_character_with_path(loc, search_loc)
-	elif creature.path.size() < 2:
-		if loc.cell != creature.start_cell:
-			var start_loc = map.get_location(creature.start_cell)
-			map.move_character_with_path(loc, start_loc)
-
-	elif loc.cell != creature.get_path_cell():
-		var path_loc = map.get_location(creature.get_path_cell())
-		map.move_character_with_path(loc, path_loc)
-
-	else:
-		_follow_path(map, loc, creature)
+		if candle_loc and candle_loc.item.is_lit:
+			if candle_loc == loc:
+				candle_loc.item.destroy()
+				creature.has_candle = true
+				creature.candle.visible = true
+				creature.candle._fire(false)
+			else:
+				map.move_character_with_path(loc, candle_loc)
+		else:
+			if loc.cell == creature.get_path_cell():
+				_follow_path(map, loc, creature)
+			else:
+				map.move_character_with_path(loc, map.get_location(creature.get_path_cell()))
 
 
 func _follow_path(map: Map, loc: Location, creature: Creature) -> void:
